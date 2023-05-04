b9c0e8ff3c1f8352d8833902a560463385b26036
Emmanuel Olea <yukiteru97@gmail.com>
2023-05-04 12:07:18 -0600

    se crea la view de la calculadora y declaran variables

---
+++ app/src/main/java/com/example/imcapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.imcapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et1, et2;
+    TextView tv1;
+    Button btn1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et1=findViewById(R.id.et1);
+        et2=findViewById(R.id.et2);
+        tv1=findViewById(R.id.tv1);
+        btn1=findViewById(R.id.btn1);
+    }
+
+    public void CalcularIMC(){
+
+    }
+}
